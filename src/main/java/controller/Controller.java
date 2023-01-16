3cd432e0076aed0cd78c8e23687bbe45a1c75fc7
Gabriel <gspadovesi@gmail.com>
2023-01-16 20:18:08 -0300

    Criando class DAO

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,11 +1,15 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import model.DAO;
+import model.JavaBeans;
 
 /**
  * Servlet implementation class Controller
@@ -13,6 +17,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns= {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	/** Criando objetos da camada model - (DAO e JavaBeans) **/
+	
+	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -28,6 +37,8 @@ public class Controller extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		dao.testeConexao();
 	}
 
 }
