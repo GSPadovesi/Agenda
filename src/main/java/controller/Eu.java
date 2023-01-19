c753b6bcda61525dec3fb84cb1e99075cd8c7269
Gabriel <gspadovesi@gmail.com>
2023-01-19 17:51:00 -0300

    Finalizando o projeto

---
+++ src/main/java/controller/Eu.java
@@ -0,0 +1,13 @@
+package controller;
+
+public class Eu {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Gabriel Santana Padovesi");
+		System.out.println("Linkedin: https://www.linkedin.com/in/gabriel-santana-padovesi-59b1041b7/");
+		System.out.println("GitHub: https://github.com/GabrielSantanaP");
+
+	}
+
+}
